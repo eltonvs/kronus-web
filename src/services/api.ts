704d3876a51e29627fb7e6db0b9cc8b935080cc3
Elton Viana <eltonvs@outlook.com>
2019-11-17 19:13:29 -0500

    Add heruku server url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
+const baseURL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8000/api'
+    : 'https://kronus-api.herokuapp.com/api'
+
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL,
 })
 
 export default api
